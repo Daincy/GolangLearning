138e2282a2bfdd304834e028f9df0baf1d8ff330
Daincy <daincy.ai@gmail.com>
2020-02-25 12:46:00 +0800

    fix bug

--- student.go
+++ student.go
@@ -35,14 +35,15 @@ func (s *studentMgr)modifyStudent(newStu *student){
 	for i, v := range s.allStudent{
 		if newStu.id == v.id{ //当学号相同时表示找到了对应的学生
 			s.allStudent[i] = newStu //根据切片索引直接把新学生赋值
+			return
 		}
 	}
 	//如果没有找到学生
-	fmt.Println("系统中没有找到学号为%d对应的学生\n", newStu.id)
+	fmt.Printf("系统中没有找到学号为 %d 对应的学生\n", newStu.id)
 }
 //展示学生
 func (s *studentMgr)showStudent(){
 	for _, v := range s.allStudent{
-		fmt.Println("学号：%d 姓名：%s 班级：%s \n", v.id, v.name, v.class )
+		fmt.Printf("学号：%d 姓名：%s 班级 %s \n\n", v.id, v.name, v.class )
 	}
 } 
